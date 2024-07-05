ef9aec2beb67abae8054e6127944b51c1516b4ec
dowofox <dhqxlajtm103@gmail.com>
2024-07-05 20:59:10 +0900

    firstcommit

--- python.py
+++ python.py
@@ -136,3 +136,4 @@ print('개발 언어에는',python,',',java,'등이 있어요')
 #문자열 포멧
 print('개발 언어에는 {},{} 등이 있어요'.format(python,java))
 print('개발 언어에는 {0},{1} 등이 있어요'.format(python,java))
+print(f'개발 언어에는 {python},{java} 등이 있어요')
