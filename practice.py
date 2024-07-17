c5d2f0e3c0da84fd550af549628a744df030780e
dowofox <dhqxlajtm103@gmail.com>
2024-07-18 04:50:17 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,84 @@
+#1
+print('Hello World')
+#2
+print("Mary's cosmetics")
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+#4
+print('C:\\Windows')
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#6
+print('오늘은','일요일')
+#7
+print('naver','kakao','sk','samsung',sep=';')
+#8
+print('naver','kakao','sk','samsung',sep = '/')
+#9
+print("first",end=""); print("second")
+#10
+print(5/3)
+#11
+삼성전자 = 50000
+총평가금액 = 삼성전자*10
+print(총평가금액)
+#12
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+#13
+s = 'hello'
+t = 'python'
+print(s+'!',t)
+#14
+2+2*3 
+#15
+a = "132" 
+print(type(a))
+#16
+num_str = "720"
+print(int(num_str))
+#17
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+#18
+num = "15.79"
+num_float = float(num)
+print(num_float)
+#19
+year = '2020'
+year1 = int(year)
+print(year1)
+print(year1-1)
+print(year1-2)
+print(year1-3)
+#20
+air = 48584
+air1 = air*36
+print(air1,"원")
+#21
+letters = 'python'
+print(letters[0],letters[2])
+#22
+license_plate = '24가 2210'
+print(license_plate[-4:])
+#23 (x)
+string = '홀짝홀짝홀짝'
+print(string[::2])
+#24 (x)
+string = 'PYTHON'
+print(string[::-1])
+#25 
+phone_number = "010-1111-2222"
+phone_number1 = phone_number.replace("-"," ")
+print(phone_number1)
+#26
+phone_number = "010-1111-2222"
+phone_number1 = phone_number.replace('-','')
+print(phone_number1)
+#27 (x)
+ur1 = "http"
