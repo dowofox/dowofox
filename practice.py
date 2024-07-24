4c86ec10cdf667b8165ee095a8b253588829da77
dowofox <dhqxlajtm103@gmail.com>
2024-07-24 11:32:47 +0900

    frist commit

--- practice.py
+++ practice.py
@@ -267,4 +267,61 @@ print(a,b,c)
 #80 (x)
 data = tuple(range(2,100,2))
 print(data)
-#81
+#81 (x)
+scores = [8.8,8.9,8.7,9.2,9.3,9.7,9.9,7.8,9.4]
+*valid_score, _, _= scores
+print(valid_score)
+#82
+scores = [8.8,8.9,8.7,9.2,9.3,9.7,9.9,9.5,7.8,9.4]
+a,b,*valid_score= scores
+print(valid_score)
+#83
+scores = [8.8,8.9,8.7,9.2,9.3,9.7,9.9,9.5,7.8,9.4]
+a,*valid_score,b= scores
+print(valid_score)
+#84
+temp = { }
+#85
+ice = {'메로나':1000,'폴라포':1200,'빵빠레':1800}
+print(ice)
+#86
+ice['죠스바'] = 1200
+ice['월드콘'] = 1500
+print(ice)
+#87
+ice = {'메로나':1000,
+       '폴라포':1200,
+       '빵빠레':1800,
+       '죠스바':1200,
+       '월드콘':1500}
+print('메로나 가격:',ice['메로나'])
+#88
+ice = {'메로나':1000,
+       '폴라포':1200,
+       '빵빠레':1800,
+       '죠스바':1200,
+       '월드콘':1500}
+ice['메로나'] = 1300
+print('메로나 가격:',ice['메로나'])
+#89
+del ice['메로나']
+print(ice)
+#90
+icecream = {'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+icecream['누가바'] = 1000
+print(icecream)
+#91
+inventory = {'메로나':[300,20],
+             '비비빅':[400,3],
+             '죠스바':[250,100]}
+#92
+print(inventory['메로나'][0],'원')
+#93
+print(inventory['메로나'][1],'개')
+#94
+inventory['월드콘'] = [500,7]
+print(inventory)
+#95
+icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+key = list(icecream.keys())
+print(key)
