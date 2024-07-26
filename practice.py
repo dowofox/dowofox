4ea2df464ba3ec734fa9368e40d417f05c3f3ead
dowofox <dhqxlajtm103@gmail.com>
2024-07-26 22:58:05 +0900

    firstcommit

--- practice.py
+++ practice.py
@@ -325,3 +325,15 @@ print(inventory)
 icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
 key = list(icecream.keys())
 print(key)
+#96
+icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+val = list(icecream.values())
+print(val)
+#97
+icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+print(sum(icecream.values()))
+#98
+icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+new_product = {'팥빙수':2700,'아맛나':1000}
+icecream.update(new_product)
+print(icecream)
